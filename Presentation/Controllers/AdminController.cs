3bd062abc51e0fff167b4c3c9b30528ade06b50c
Matthias Scicluna <sciclunamatthias@gmail.com>
2024-01-05 15:45:20 +0100

    Added Admin Ticket List

--- Presentation/Controllers/AdminController.cs
+++ Presentation/Controllers/AdminController.cs
@@ -1,9 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using DataAccess.Repositories;
+using Presentation.Models.ViewModels;
 
 namespace Presentation.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly TicketDBRepository _ticketRepository;
+
+        public AdminController(TicketDBRepository ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+
+        public IActionResult ListTickets()
+        {
+            var tickets = _ticketRepository.GetTicket().ToList();
+            var viewModelList = tickets.Select(t => new AdminTicketListViewModel
+            {
+                Name = t.Name,
+                Surname = t.Surname,
+                Row = t.Row,
+                Column = t.Column,
+                Cancelled = t.Cancelled,
+                PassportNo = t.PassportNo,
+                PricePaid = t.PricePaid
+
+
+            }).ToList();
+
+            return View(viewModelList);
+        }
+        
         public IActionResult Index()
         {
             if (User.Identity.IsAuthenticated == false)
@@ -19,3 +48,4 @@ namespace Presentation.Controllers
         }
     }
 }
+
---
+++ Presentation/Models/ViewModels/AdminTicketListViewModel.cs
@@ -0,0 +1,45 @@
+﻿using DataAccess.Repositories;
+using Domain.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Presentation.Models.ViewModels
+{
+    public class AdminTicketListViewModel
+    {
+
+        public AdminTicketListViewModel() { }
+
+
+        public AdminTicketListViewModel(FlightDbRepository flightDbRepository) 
+        {
+            Flights = flightDbRepository.GetFlights();
+
+        }
+
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public IQueryable<Flight> Flights { get; set; }
+        public int FlightIdFK { get; set; }
+
+        [Display(Name = "Passport Number")]
+        public string PassportNo { get; set; }
+        public IFormFile Passport { get; set; }
+        public double PricePaid { get; set; }
+        public bool Cancelled { get; set; }
+
+        public Flight SelectedFlight { get; set; }
+
+
+        //public Dictionary<int, Dictionary<(int, int), bool>> SeatAvailability { get; set; }
+
+
+    }
+}
